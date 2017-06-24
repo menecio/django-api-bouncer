c0cbdcd95ad2ff4c70fa6184b107066d51a4a276
Aristobulo Meneses <aristobulo@gmail.com>
2017-06-24 14:14:03 +0200

    Lint

--- api_bouncer/__init__.py
+++ api_bouncer/__init__.py
@@ -1,5 +1,5 @@
-#import pkg_resources
+# import pkg_resources
 
-#__version__ = pkg_resources.require('django-api-bouncer')[0].version
+# __version__ = pkg_resources.require('django-api-bouncer')[0].version
 
 default_app_config = 'api_bouncer.apps.ApiBouncerConfig'

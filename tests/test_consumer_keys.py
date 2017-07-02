c38929e15a5429233e7678b606790361d49790d9
Aristobulo Meneses <aristobulo@gmail.com>
2017-07-02 12:09:24 +0200

    Add test cases for ConsumerKey object

---
+++ tests/test_consumer_keys.py
@@ -0,0 +1,60 @@
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth import get_user_model
+
+from api_bouncer.models import Consumer, ConsumerKey
+
+User = get_user_model()
+
+
+class ConsumerKeyTests(APITestCase):
+    def setUp(self):
+        self.superuser = User.objects.create_superuser(
+            'john',
+            'john@localhost.local',
+            'john123john'
+        )
+        self.user = User.objects.create_user(
+            'jane',
+            'jane@localhost.local',
+            'jane123jane'
+        )
+        self.consumer = Consumer.objects.create(username='django')
+
+        self.url = '/consumers/{}/key-auth/'
+
+    def test_create_consumer_key_auto(self):
+        """
+        Ensure we can create a new consumer key object, with a default value.
+        """
+        self.client.login(username='john', password='john123john')
+        url = self.url.format(self.consumer.username)
+
+        response = self.client.post(url, {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(ConsumerKey.objects.count(), 1)
+        self.assertEqual(ConsumerKey.objects.get().consumer.username, 'django')
+
+    def test_create_consumer_key_auto_403(self):
+        """
+        Ensure we can create a new consumer key object with a default value,
+        only as superusers.
+        """
+        url = self.url.format(self.consumer.username)
+
+        response = self.client.post(url, {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_create_consumer_key_given_key(self):
+        """
+        Ensure we can create a new consumer key object, with a default value.
+        """
+        data = {'key': 'abc123456'}
+        self.client.login(username='john', password='john123john')
+        url = self.url.format(self.consumer.username)
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(ConsumerKey.objects.count(), 1)
+        self.assertEqual(ConsumerKey.objects.get().consumer.username, 'django')
+        self.assertEqual(ConsumerKey.objects.get().key, data['key'])
